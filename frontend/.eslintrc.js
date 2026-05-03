1c102d464c533e0038e03321c9106f96e8ebd77a
Hwang-Jeong-Seon <jshwang370@gmail.com>
2026-05-03 19:49:07 +0900

    FE: 미사용 파일 삭제 및 package 업데이트

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:vue/recommended",
     "eslint:recommended",
     "plugin:prettier/recommended",
-    "prettier" // Prettier와 충돌 방지
+    "prettier", // Prettier와 충돌 방지
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
@@ -15,6 +15,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": "off" // 마감 기한 내 빠른 개발을 위해 컴포넌트명 규칙 완화
+    "vue/multi-word-component-names": "off",
   },
 };
